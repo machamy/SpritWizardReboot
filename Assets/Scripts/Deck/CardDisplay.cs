cbbc62fc1823eefb9f8c7821e8db073d1437b0af
villainouskirby <69393156+villainouskirby@users.noreply.github.com>
2025-01-01 17:34:15 +0900

    덱빌딩(드로우) 기능

--- Assets/Scripts/Deck/CardDisplay.cs
+++ Assets/Scripts/Deck/CardDisplay.cs
@@ -1,12 +1,28 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class CardDisplay : MonoBehaviour
 {
-    
+    [SerializeField] private TextMeshProUGUI nameText;
+    [SerializeField] private TextMeshProUGUI descriptionText;
+    [SerializeField] private Image image;
+    [SerializeField] private TextMeshProUGUI costText;
+    [SerializeField] private TextMeshProUGUI damageText;
+
+    public void DisplayCard(CardSO card)
+    {
+        nameText.text = card.cardName;
+        descriptionText.text = card.description;
+        image.sprite = card.image;
+        costText.text = card.cost.ToString();
+        damageText.text = card.damage.ToString();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
