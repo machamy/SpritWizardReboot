39109071a987db15c0e6bfbdec94546c8604351e
hance0103 <kmhosu0103@naver.com>
2025-01-01 17:34:03 +0900

    몬스터 구조

--- Assets/Scripts/Game/Enemy/EnemyBehavior.cs
+++ Assets/Scripts/Game/Enemy/EnemyBehavior.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class EnemyBehavior
+public class EnemyBehavior : ScriptableObject
 {
     
 }
---
+++ Assets/Scripts/Game/Enemy/EnemyRest.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyRest", menuName = "Enemy/EnemyRest")]
+public class EnemyRest : EnemyBehavior
+{
+    
+}
