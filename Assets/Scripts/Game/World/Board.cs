d6420f09b2e22e1bfc7ce2a030b3e86f82b331c6
KimSeungWoo <macham2001@gmail.com>
2025-01-01 16:44:19 +0900

    보드 헬퍼메소드 추가

--- Assets/Scripts/Game/World/Board.cs
+++ Assets/Scripts/Game/World/Board.cs
@@ -8,17 +8,51 @@ using UnityEngine;
 public class Board : MonoBehaviour
 {
      [SerializeField] private Vector2Int gridSize;
+     public Vector2Int GridSize => gridSize;
      private Tile[][] _tilemap;
      private Grid _grid;
-     private void Awake()
-     {
-          
-     }
      
      public void Initialize(Tile[][] tilemap)
      {
           _tilemap = tilemap;
           gridSize = new Vector2Int(tilemap.Length, tilemap[0].Length);
+     }
+
+     /// <summary>
+     /// world position을 바탕으로, 타일을 받아온다. 범위 밖이면 null
+     /// </summary>
+     /// <param name="worldPos"></param>
+     /// <returns></returns>
+     public Tile WorldToTile(Vector3 worldPos)
+     {
+          Vector3Int cell = _grid.WorldToCell(worldPos);
+          if (cell.x < 0 || cell.x >= gridSize.x || cell.y < 0 || cell.y >= gridSize.y)
+          {
+               Debug.Log($"Cell out of bounds {cell} (input : {worldPos})");;
+               return null;
+          }
+          return _tilemap[cell.x][cell.y];
+     }
+     
+     /// <summary>
+     /// 월드 좌표 -> 셀 좌표
+     /// </summary>
+     /// <param name="worldPos"></param>
+     /// <returns></returns>
+     public Vector2Int WorldToCell(Vector3 worldPos)
+     {
+          Vector3Int cell = _grid.WorldToCell(worldPos);
+          return new Vector2Int(cell.x, cell.y);
+     }
+     
+     /// <summary>
+     /// 셀 좌표 -> 월드 좌표
+     /// </summary>
+     /// <param name="cell"></param>
+     /// <returns></returns>
+     public Vector3 CellToWorld(Vector2Int cell)
+     {
+          return _grid.CellToWorld(new Vector3Int(cell.x, cell.y, 0));
      }
      
      #if UNITY_EDITOR
