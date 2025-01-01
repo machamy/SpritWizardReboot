041bbc75b1baa2e856ee225e78a80d36647a41c4
machamy <macham2001@gmail.com>
2025-01-02 04:01:10 +0900

    게임매니저 문 체력

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,47 @@
+﻿
+using DefaultNamespace;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    
+    private static GameManager _instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+              if (_instance == null)
+                {
+                    _instance = FindObjectOfType<GameManager>();
+                    if (_instance == null)
+                    {
+                        GameObject go = new GameObject("GameManager");
+                        _instance = go.AddComponent<GameManager>();
+                    }
+                }
+                return _instance;   
+        }
+    }
+    
+    
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private FloatVariableSO gateHP;
+    public float GateHP
+    {
+        get => gateHP.Value;
+        set => gateHP.Value = value;
+    }
+}
