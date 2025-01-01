d4daf741c1c9476e8538ab5505294c2b294e33dc
hance0103 <kmhosu0103@naver.com>
2025-01-02 07:36:46 +0900

    몬스터 투사체 공격판정 추가 (인터페이스 추가해야함)

--- Assets/Scripts/Game/Enemy/EnemyProjectile.cs
+++ Assets/Scripts/Game/Enemy/EnemyProjectile.cs
@@ -5,6 +5,7 @@ public class EnemyProjectile : MonoBehaviour
 {
     private Entity _entity;
     public TurnEventChannelSO enemyTurnEvent;
+    public TurnEventChannelSO playerTurnExitEvent;
     public int _dmg { get; set; }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -32,5 +33,19 @@ public class EnemyProjectile : MonoBehaviour
             GameManager.Instance.GateHP -= _dmg;
             Destroy(gameObject);
         }
+    }
+
+    public void OnHit(int dmg)
+    {
+        playerTurnExitEvent.OnTurnEventRaised += OnPlayerturnEnd;
+    }
+    private void OnPlayerturnEnd(int turn)
+    {
+        playerTurnExitEvent.OnTurnEventRaised -= OnPlayerturnEnd;
+        Destroy(gameObject);
+    }
+    private void Ondeath()
+    {
+        //애니메이션 출력
     }
 }
