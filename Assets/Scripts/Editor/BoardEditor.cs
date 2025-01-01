aea2f30ce617b17894501c2b5d3b1a549559db61
KimSeungWoo <macham2001@gmail.com>
2025-01-01 16:00:10 +0900

    디렉토리 기초 추가

---
+++ Assets/Scripts/Editor/BoardEditor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEditor;
+
+namespace Editor
+{
+    
+    [CustomEditor(typeof(Board))]
+    public class BoardEditor : UnityEditor.Editor
+    {
+        
+        private SerializedProperty gridSize;
+        
+        private void OnEnable()
+        {
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            serializedObject.Update();
+        }
+    }
+}
---
+++ Assets/Scripts/Game/World/Board.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+     private Vector2Int gridSize;
+     
+     
+     
+     #if UNITY_EDITOR
+     public void CreateBoardBase()
+     {
+          
+     }
+     
+     #endif
+     
+}
