939f7473169880f853bbd4a1b89229c3a3f8601c
machamy <macham2001@gmail.com>
2025-01-07 12:42:33 +0900

    직렬화 가능한 딕셔너리

---
+++ Assets/Scripts/DataStructure/SerializableDict.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace DataStructure
+{
+    [Serializable]
+    public class SerializableDict<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
+    {
+        [SerializeField] private TKey _defaultKey;
+        [Serializable]
+        public class DictPair<TKey, TValue>
+        {
+            public TKey Key;
+            public TValue Value;
+        }
+
+        [SerializeField] private List<DictPair<TKey, TValue>> _pairs = new ();
+
+
+        public void OnBeforeSerialize()
+        {
+            _pairs.Clear();
+            int i = 0;
+            foreach (var pair in this)
+            {
+                _pairs.Add(new DictPair<TKey, TValue>() { Key = pair.Key, Value = pair.Value });
+                i++;
+            }
+        }
+
+        public void OnAfterDeserialize()
+        {
+            this.Clear();
+            foreach (var pair in _pairs)
+            {
+                if (pair.Key == null || this.ContainsKey(pair.Key))
+                {
+                    pair.Key = _defaultKey;
+                }
+                this[pair.Key] = pair.Value;
+            }
+        }
+
+
+    }
+}
