63958cc83e4e1c761c36fbc5159d53d814d1d1a2
machamy <macham2001@gmail.com>
2025-01-07 10:26:35 +0900

    전투관리 클래스

---
+++ Assets/Scripts/Game/Singleton/BattleManager.cs
@@ -0,0 +1,43 @@
+﻿using Game.Player;
+using UnityEngine;
+
+namespace Game
+{
+    public class BattleManager : Singleton<BattleManager>
+    {
+        public Board Board => board;
+        [Header("References")]
+        [SerializeField] private Board board;
+        [Header("Slime")]
+        [SerializeField] private Slime iceSlime;
+        [SerializeField] private Slime grassSlime;
+        [SerializeField] private Slime fireSlime;
+        [Header("Deck")]
+        [SerializeField] private Deck startDeck;
+        [SerializeField] private Deck drawDeck;
+        [SerializeField] private Deck discardDeck;
+        public Deck StartDeck => startDeck;
+        public Deck DrawDeck => drawDeck;
+        public Deck DiscardDeck => discardDeck;
+        [Header("Battle")]
+        [SerializeField] private bool isOnBattle = false;
+        public bool IsOnBattle => isOnBattle;
+        
+        private void Awake()
+        {
+            board = FindAnyObjectByType<Board>();
+        }
+        
+        
+        public Slime GetSlime(SkillCaster caster)
+        {
+            return caster switch
+            {
+                SkillCaster.Ice => iceSlime,
+                SkillCaster.Grass => grassSlime,
+                SkillCaster.Fire => fireSlime,
+                _ => null
+            };
+        }
+    }
+}
