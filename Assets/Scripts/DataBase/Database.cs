f60c5e9e670e09f3c633a16d90ed5bc385d4b4a6
machamy <macham2001@gmail.com>
2025-01-07 10:24:42 +0900

    내부적으로 사용할 데이터베이스 클래스

---
+++ Assets/Scripts/DataBase/Database.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+using DataBase.DataClasses;
+using UnityEngine;
+
+
+public class Database : MonoBehaviour
+{
+    public bool isLoaded = false;
+    public static List<CardData> allCards { get; private set; } = new List<CardData>();
+    public static List<MagicCardData> allMagicCards { get; private set; } = new List<MagicCardData>();
+    
+    [Header("Data")]
+    [SerializeField] private GoogleSheetSO googleSheetSO;
+    [Header("Factory")]
+    [SerializeField] private MagicCardDataFactorySO magicCardDataFactory;
+    private void Awake()
+    {
+        if(isLoaded)
+            return;
+        isLoaded = true;
+        LoadData();
+    }
+    
+    private void LoadData()
+    {
+        foreach (var cardData in googleSheetSO.RawMagicCardList)
+        {
+            allMagicCards.Add(magicCardDataFactory.CreateMagicCard(cardData));
+        }
+        
+        foreach (var cardData in allMagicCards)
+        {
+            allCards.Add(cardData);
+        }
+    }
+    
+}
